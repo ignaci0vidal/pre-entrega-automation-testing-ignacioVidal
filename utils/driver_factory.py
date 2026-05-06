ed73b2c07805f77b0cd605acfbc3eb632f3f2eeb
ignacio vidal <ignaciovidal@gmail.com>
2026-05-06 19:24:05 -0300

    Configura Chrome en modo incognito

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -5,11 +5,12 @@ from selenium.webdriver.chrome.options import Options
 def crear_driver():
     """
     Crea y configura una instancia de Chrome WebDriver.
-    Se utiliza una función separada para reutilizar la configuración
-    en todos los tests del proyecto.
+    Se utiliza modo incógnito para evitar guardar sesión,
+    usuario, contraseña o datos de navegación entre ejecuciones.
     """
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--incognito")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(2)
